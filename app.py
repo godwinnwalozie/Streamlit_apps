9fccc63e2dcd11d30fd8bb2f3ada477a44857ca1
Godwin <42586445+Godwinnwalozie@users.noreply.github.com>
2022-07-04 20:29:46 +0100

    s

--- app.py
+++ app.py
@@ -9,226 +9,304 @@ from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import LabelEncoder
 import os
 
+
+
 st.set_page_config(layout="wide")
 m = st.markdown("""
 <style>
 div.stButton > button:first-child {
-    background-color: #0099ff;
+    background-color: #0068C9 ;
     color:#ffffff;
 }
 div.stButton > button:hover {
-    background-color: #00ff00;
-    color:#ff0000;
+    background-color: #00802b;
+    color:#ccffff;
     }
 </style>""", unsafe_allow_html=True)
 
+
+# Remove whitespace from the top of the page and sidebar
+st.markdown("""
+        <style>
+               .css-18e3th9 {
+                    padding-top: 2.5rem;
+                    padding-bottom: 5rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+               .css-hxt7ib{
+                    padding-top: 3rem;
+                    padding-right: 2rem;
+                    padding-bottom: 2rem;
+                    padding-left: 2rem;
+                    color: rgb(247 247 250);
+                }
+                .css-1siy2j7 {
+                    background-color: rgb(0 104 201);
+                }
+                .css-zx8yrj {
+                    background-color: #f2f7fe;
+                }
+                .css-qrbaxs {
+                    font-size: 14px;
+                    color: rgb(241 241 241);
+                }
+                .st-br {
+                    color: black;
+                }
+                .css-gdzsw5 {
+                    font-weight: 600;
+                }
+                .css-a51556 {
+                    font-weight: 900;
+                    color: ash;
+}         
+        </style>
+        """, unsafe_allow_html=True)
+
+
+
+
+
+
+BACKGROUND_COLOR = 'white'
+COLOR = 'black'
+
+def set_page_container_style(
+        max_width: int = 1100, max_width_100_percent: bool = False,
+        padding_top: int = 1, padding_right: int = 10, padding_left: int = 1, padding_bottom: int = 10,
+        color: str = COLOR, background_color: str = BACKGROUND_COLOR,
+    ):
+        if max_width_100_percent:
+            max_width_str = f'max-width: 100%;'
+        else:
+            max_width_str = f'max-width: {max_width}px;'
+        st.markdown(
+            f'''
+            <style>
+                .reportview-container .css-1lcbmhc .css-1outpf7 {{
+                    padding-top: 35px;
+                }}
+                .reportview-container .main .block-container {{
+                    {max_width_str}
+                    padding-top: {padding_top}rem;
+                    padding-right: {padding_right}rem;
+                    padding-left: {padding_left}rem;
+                    padding-bottom: {padding_bottom}rem;
+                }}
+                .reportview-container .main {{
+                    color: {color};
+                    background-color: {background_color};
+                }}
+            </style>
+            ''',
+            unsafe_allow_html=True,
+        )
 sns.set_theme(font_scale=0.7, style="darkgrid")
 
-
-image = Image.open(r'C:\Users\godwi\GitHub\streamlit_app_stroke_precdict\data\header_image.jpg')
-st.image(image)
-
-#warnings.filterwarnings(action='ignore', category=DataConversionWarning)
-
-#Import file
-#xlsx_file = st.sidebar.file_uploader('Import .xlsx File', type = 'xlsx')
-
-
-
-#def main():
-    #-------------------------------Sisdebar-----------------------
-    #with st.sidebar.container():
-        #image = Image.open( )
-        #st.image(image)
-
-
-st.markdown("<h1 style='text-align: left; color: black;'>Stroke Disease Prediction</h1>", unsafe_allow_html=True)
-st.write(" **By Godwin Nwalozie : July 2022** ")
-#st.markdown("***")
-
 with st.container():
-    st.info(" This Machine Learning algorithm tries to predict whether a patient is likely to get stroke based on the feature\
-                            parameters like gender, age, various diseases, and smoking status. Each row in the \
-                            data provides relavant information about the patient.  \n **Data Source** :\
+   
+    header_image = os.path.abspath("GitHub/streamlit_app_stroke_precdict/data/top_header_image.png")
+    image_header = Image.open(header_image)
+    st.image(image_header)
+    
+
+st.write(" **Developed by : Godwin Nwalozie**")
+#st.markdown("<h1 style='text-align: left; color: black;'>Stroke Disease Prediction</h1>", unsafe_allow_html=True)
+#st.write("#### Godwin Nwalozie July 2022 ")
+#st.markdown("***")
+#st.markdown("***")
+with st.container():
+    st.info(" ###### Based on criteria such as gender, age, heart disease, and smoking status, \
+        this Machine Learning system attempts to predict whether a patient is likely to have a stroke. \
+            Each row of data contains pertinent information about the patient.  \n **Data Source** :\
                             ***https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset*** ")
 
-    #st.markdown("***")
+st.markdown("") 
+
     #st.write ('### Stroke Disease Prediction App')
 
+    # The dataset for the estimator
+dataframe = os.path.abspath("GitHub/streamlit_app_stroke_precdict/data/master_df.csv")
+master_df= pd.read_csv(dataframe)
+
+    
     # Store inputs into dataframe
+select_features = os.path.abspath("GitHub/streamlit_app_stroke_precdict/data/select_features.png")
+select_features = Image.open(select_features)
+st.sidebar.image(select_features)
+# st.sidebar.write('### Select Features')
+gender = st.sidebar.selectbox('Whats your gender?',('Male', 'Female'))
+age = st.sidebar.number_input('Input Age ', key = 'int',max_value  =100,min_value = 18) #st.sidebar.slider('Age',0,50,100)
+hypertension = st.sidebar.selectbox('Are you hpertensive? ',("Yes", "No"))
+heart_disease = st.sidebar.selectbox('Any heart related disease ? ',("Yes", "No"))
+#work_type = st.sidebar.selectbox('Work type ?', ("Private" ,"Self-employed","Children","Govt_job ","Never_worked"))
+Residence_type = st.sidebar.selectbox('Residencial Type ', ("Urban","Rural"))
+avg_glucose_level= st.sidebar.number_input('Average Gloucose Level', min_value= 0 , max_value=140)
+bmi = st.sidebar.number_input('Enter your current BMI', min_value= 0, max_value= 100)
+smoking_status = st.sidebar.selectbox('Smoking status',("Never smoked" , "Unknown","formerly smoked","Smokes","Never_smoked"))
+#st.sidebar.markdown("***")
 
-    master_df= pd.read_csv(r"C:\Users\godwi\GitHub\streamlit_app_stroke_precdict\data\master_df.csv")
 
-    st.sidebar.title(' Select Features')
-    gender = st.sidebar.selectbox('Whats your gender?',('Male', 'Female'))
-    age = st.sidebar.number_input('Input Age ', key = 'int',max_value  =100,min_value = 18) #st.sidebar.slider('Age',0,50,100)
-    hypertension = st.sidebar.selectbox('Are you hpertensive? ',("Yes", "No"))
-    heart_disease = st.sidebar.selectbox('Any heart related disease ? ',("Yes", "No"))
-    #work_type = st.sidebar.selectbox('Work type ?', ("Private" ,"Self-employed","Children","Govt_job ","Never_worked"))
-    Residence_type = st.sidebar.selectbox('Residencial Type ', ("Urban","Rural"))
-    avg_glucose_level= st.sidebar.number_input('Average Gloucose Level', min_value= 0 , max_value=140)
-    bmi = st.sidebar.number_input('Enter your current BMI', min_value= 0, max_value= 100)
-    smoking_status = st.sidebar.selectbox('Smoking status',("Never smoked" , "Unknown","formerly smoked","Smokes","Never_smoked"))
-    st.sidebar.markdown("***")
 
-    features = {"gender": gender,"age":age,"hypertension": hypertension, "heart_disease":heart_disease, "Residence_type" : Residence_type,
+
+features = {"gender": gender,"age":age,"hypertension": hypertension, "heart_disease":heart_disease, "Residence_type" : Residence_type,
                 "avg_glucose_level" : avg_glucose_level, "bmi" : bmi,"smoking_status": smoking_status }
-    show = pd.DataFrame(features, index= [0])
-    show.age= show.age.astype('int')
-    upper  = [i.upper() for i in show]
-    show.columns = upper
+show = pd.DataFrame(features, index= [0])
+show.age= show.age.astype('int')
+upper  = [i.capitalize() for i in show]
+show.columns = upper
+    
+row_count = len(master_df)
+col_count_ini = len(master_df.columns)
+col_count = len(master_df.columns)-3
+st.markdown("<h4 style='text-align: left; color: chocolate;'> Details of the trained dataset </h4>", unsafe_allow_html=True)
 
-        #st.markdown("***")
-    st.markdown("***")
-
-    row_count = len(master_df)
-    col_count_ini = len(master_df.columns)
-    col_count = len(master_df.columns)-3
-    st.markdown("<h3 style='text-align: left; color: chocolate;'> Dataset info and Deployed Estimator </h3>", unsafe_allow_html=True)
-
-
+with st.container():
     col1, col2, col3,col4,col5 = st.columns(5)
     col1.metric("Estimator","RFClassifier", "+")
     col2.metric("Model Prediction Accuracy", "90%", "<>")
     col3.metric("Number of rows trained", row_count, "<>")
     col4.metric("Initial No of columns", col_count_ini, "<>")
     col5.metric("No of columns trained", col_count, "-")
+st.markdown("***")
 
-
-
-    st.info (' ###### **Your Selected Features**')
+with st.container():
+    
+    st.markdown("<h4 style='text-align: left; color: chocolate;'> Your selected features </h4>", unsafe_allow_html=True)
     st.table(show)
+    
 
+inputed = pd.DataFrame(data = features, index = [0])
+le = LabelEncoder()
+inputed.gender =le.fit_transform(inputed.gender)
+#inputed.work_type =le.fit_transform(inputed.work_type)
+inputed.Residence_type =le.fit_transform(inputed.Residence_type)
+inputed.smoking_status =le.fit_transform(inputed.smoking_status)
+inputed.hypertension =le.fit_transform(inputed.hypertension)
+inputed.heart_disease =le.fit_transform(inputed.heart_disease)
+inputed.bmi = inputed.bmi.apply(lambda x: round(x,2))
+     
 
-
-    inputed = pd.DataFrame(data = features, index = [0])
-
-    stroke_model = pickle.load(open(r'C:\Users\godwi\GitHub\streamlit_app_stroke_precdict\data\estimator_pkl', 'rb'))
-
-    le = LabelEncoder()
-    inputed.gender =le.fit_transform(inputed.gender)
-    #inputed.work_type =le.fit_transform(inputed.work_type)
-    inputed.Residence_type =le.fit_transform(inputed.Residence_type)
-    inputed.smoking_status =le.fit_transform(inputed.smoking_status)
-    inputed.hypertension =le.fit_transform(inputed.hypertension)
-    inputed.heart_disease =le.fit_transform(inputed.heart_disease)
-    inputed.bmi = inputed.bmi.apply(lambda x: round(x,2))
-
-
-
-    prediction = stroke_model.predict(inputed)
-    if prediction [0] == 0:
-        prediction =  "Low Risk "
+trained_model = os.path.abspath("GitHub/streamlit_app_stroke_precdict/data/estimator_pkl")
+stroke_model = pickle.load(open(trained_model, 'rb'))
+         
+prediction = stroke_model.predict(inputed)
+if prediction [0] == 0:
+    prediction =  "Low Risk" 
         #st.balloons()
-    else:
-        prediction = "High Risk"
-    if st.button("Click to Make Prediction"):
-        # Output prediction
-        st.success(f"Prediction : {prediction}")
-    else:
-        st.write('###### **Prediction display here !!**')
-    st.markdown("")
+else:
+    prediction = "High Risk"
+    
+if st.button("click to make prediction"):
+             
+    # Output prediction
+    st.success(f"Prediction is {prediction} ")
+else:
+    pred_smiley = os.path.abspath("GitHub/streamlit_app_stroke_precdict/data/botton_click.png")
+    pred_smiley = Image.open(pred_smiley)
+    st.image(pred_smiley,width= 400)
+    #st.write('###### **Prediction display here !!**')
 
-
-
-
+st.markdown("***")
 
 #if __name__ == "__main__":
-
-
 
       #main()
 
 with st.container():
-        #master_df= pd.read_csv("C:/Users/godwi/GitHub/streamlit_app_stroke_precdict/data/master_df.csv")
-        st.markdown("<h3 style='text-align:left; color: chocolate;'>Exploratory Data Analysis & Confusion Matrix Report</h3>",
+    #master_df= pd.read_csv("C:/Users/godwi/GitHub/streamlit_app_stroke_precdict/data/master_df.csv")
+    st.markdown("<h4 style='text-align:left; color: chocolate;'>Exploratory Data Analysis & Confusion Matrix Report</h4>",
                     unsafe_allow_html=True)
 
 
-col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-with col1:
+    with col1:
 
             #bmi age correlation
-    fig,ax = plt.subplots(figsize = (6,3.6))
-    sns.regplot(data = master_df, x= "bmi", y= "age",marker="x")
-    ax.set(title ="Correlation between Age and BMI")
-    st.write(fig)
+        fig,ax = plt.subplots(figsize = (6,3.6))
+        sns.regplot(data = master_df, x= "bmi", y= "age",marker="x")
+        ax.set(title ="Correlation between Age and BMI")
+        st.write(fig)
 
 
             #st.markdown("***")
-    fig, ax =plt.subplots(figsize = (6,2.6))
-    gender_stat = master_df.gender.value_counts().to_frame()
+        fig, ax =plt.subplots(figsize = (6,2.6))
+        gender_stat = master_df.gender.value_counts().to_frame()
+                #disease_check.plot( kind = 'bar', color = ('teal',"blueviolet"), ax=ax)
+        sns.barplot(data = gender_stat, x = gender_stat.index, y = gender_stat['gender'] ,capsize = 0.05)
+        ax.set(title ="Gender Distribution", xlabel ='gender', ylabel ='count')
+        st.write(fig)
+
+                # chart for confusion metrix
+            
+        confusion = os.path.abspath("GitHub/streamlit_app_stroke_precdict/data/conf_max_df.csv")
+        conf_max_df= pd.read_csv (confusion)
+        
+        fig,ax = plt.subplots(figsize = (6,2.4))
+        sns.heatmap(conf_max_df/np.sum(conf_max_df) ,xticklabels = True, annot =True,fmt =".2%",
+                            ax = ax,linewidths=0.2, linecolor='grey',)
+        ax.set(title ="Confusion Matrix")
+        st.write(fig)
+
+
+
+
+
+        with col2:
+
+            fig,ax =plt.subplots(figsize = (10,6))
+            feature_check =sns.heatmap(master_df.corr(), cmap = "Greens", annot = True,linewidths=0.3, linecolor='grey');
+            ax.set(title ="Feature Correlation")
+            st.write(fig)
+            #st.markdown("***")
+
+
+
+
+            age_hyper = master_df.loc[:,["age","gender"]]
+            age_hyper['age_cat'] = age_hyper.age.apply(lambda x :  "0-2" if 0 <= x<2 else
+                                                "2-5" if 2<= x<= 5 else
+                                                "6-13" if 5< x< 13 else
+                                                "13-18" if 13<= x< 18 else
+                                                "18-30" if 18<= x< 30 else
+                                                "30-40" if 30<= x< 40 else
+                                                "40-50" if 40<= x< 50 else
+                                                "50-65" if 50<= x< 65 else
+                                                "65+" if x>= 65 else "not known")
+
+
+            pivot_age = age_hyper.pivot_table(index = 'age_cat', columns='gender', values="age", aggfunc= 'count')
+
+            fig,ax = plt.subplots(figsize = (5,2.1))
+            pivot_age.plot(kind = 'bar', ax = ax)
+            ax.set(title ="Age Category - Distribution")
+            st.write(fig)
+            #st.markdown("***")
+
+
+
+
+
+            # chart for heart diseaase
+            disease_check = pd.crosstab(master_df.gender, master_df.heart_disease).rename({0: "No", 1:"Yes"}, axis = 1)
+            fig, ax =plt.subplots(figsize = (6,2.4))
             #disease_check.plot( kind = 'bar', color = ('teal',"blueviolet"), ax=ax)
-    sns.barplot(data = gender_stat, x = gender_stat.index, y = gender_stat['gender'] ,capsize = 0.05)
-    ax.set(title ="Gender Distribution", xlabel ='gender', ylabel ='count')
-    st.write(fig)
-
-            # chart for confusion metrix
-    conf_max_df= pd.read_csv ("C:/Users/godwi/GitHub/streamlit_app_stroke_precdict/data/conf_max_df.csv")
-    fig,ax = plt.subplots(figsize = (6,2.4))
-    sns.heatmap(conf_max_df/np.sum(conf_max_df) ,xticklabels = True, annot =True,fmt =".2%",
-                        ax = ax,linewidths=0.2, linecolor='grey',)
-    ax.set(title ="Confusion Matrix")
-    st.write(fig)
+            sns.heatmap(data = disease_check, annot = True, fmt ="2d", cmap = "Blues",linewidths=0.4, linecolor='grey' )
+            ax.set( xlabel = 'Has Heart disease ?')
+            ax.set(title ="Heart Disease by Gender")
+            st.write(fig)
+            #st.markdown("***")
 
 
 
+with st.container():
 
-
-    with col2:
-
-        fig,ax =plt.subplots(figsize = (10,6))
-        feature_check =sns.heatmap(master_df.corr(), cmap = "Greens", annot = True,linewidths=0.3, linecolor='grey');
-        ax.set(title ="Feature Correlation")
-        st.write(fig)
-        #st.markdown("***")
-
-
-
-
-        age_hyper = master_df.loc[:,["age","gender"]]
-        age_hyper['age_cat'] = age_hyper.age.apply(lambda x :  "0-2" if 0 <= x<2 else
-                                            "2-5" if 2<= x<= 5 else
-                                            "6-13" if 5< x< 13 else
-                                            "13-18" if 13<= x< 18 else
-                                            "18-30" if 18<= x< 30 else
-                                            "30-40" if 30<= x< 40 else
-                                            "40-50" if 40<= x< 50 else
-                                            "50-65" if 50<= x< 65 else
-                                            "65+" if x>= 65 else "not known")
-
-
-        pivot_age = age_hyper.pivot_table(index = 'age_cat', columns='gender', values="age", aggfunc= 'count')
-
-        fig,ax = plt.subplots(figsize = (5,2.1))
-        pivot_age.plot(kind = 'bar', ax = ax)
-        ax.set(title ="Age Category - Distribution")
-        st.write(fig)
-        #st.markdown("***")
-
-
-
-
-
-        # chart for heart diseaase
-        disease_check = pd.crosstab(master_df.gender, master_df.heart_disease).rename({0: "No", 1:"Yes"}, axis = 1)
-        fig, ax =plt.subplots(figsize = (6,2.4))
-        #disease_check.plot( kind = 'bar', color = ('teal',"blueviolet"), ax=ax)
-        sns.heatmap(data = disease_check, annot = True, fmt ="2d", cmap = "Blues",linewidths=0.4, linecolor='grey' )
-        ax.set( xlabel = 'Has Heart disease ?')
-        ax.set(title ="Heart Disease by Gender")
-        st.write(fig)
-        #st.markdown("***")
-
-
-
-
-
-st.write(' Thank you for visiting :-) '  )
-image = Image.open("C:/Users/godwi/Pictures/mazi2.png")
-st.image(image, width= 130)
-st.write("**Godwin**")
-
-st.write('Visit my page on Kaggle : https://www.kaggle.com/godwinnwalozie/code')
-st.write('Visit my Github page :https://github.com/godwinnwalozie')
+    st.write(' Thank you for visiting :-) '  )
+    image = Image.open("C:/Users/godwi/Pictures/mazi2.png")
+    st.image(image, width= 190)
+    st.write("**Godwin Nwalozie **")
+    st.text("“𝑾𝒊𝒕𝒉𝒐𝒖𝒕 𝒅𝒂𝒕𝒂 𝒚𝒐𝒖’𝒓𝒆 𝒋𝒖𝒔𝒕 𝒂𝒏𝒐𝒕𝒉𝒆𝒓 𝒑𝒆𝒓𝒔𝒐𝒏 𝒘𝒊𝒕𝒉 𝒂𝒏 𝒐𝒑𝒊𝒏𝒊𝒐𝒏.” 𝑬𝒅𝒘𝒂𝒓𝒅𝒔 𝑫𝒆𝒎𝒊𝒏𝒈, 𝑺𝒕𝒂𝒕𝒊𝒔𝒕𝒊𝒄𝒊𝒂𝒏")
+    st.text('Visit my page on Kaggle : https://www.kaggle.com/godwinnwalozie/code')
+    st.text ('Visit my Github page :https://github.com/godwinnwalozie')
